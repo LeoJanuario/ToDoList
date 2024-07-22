9c66decf817d2358611e1095ea9a4ffcfeb2a152
LeoJanuario <leo.januario@icloud.com>
2024-07-21 23:19:21 -0300

    Adicionar arquivos de projeto.

---
+++ ToDoList/Classes/GerenciadorDeTarefas.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Classes
+{
+    internal class GerenciadorDeTarefas
+    {
+        private List<Tarefa> Tarefas = new List<Tarefa>();
+        private int ProximoId = 1;
+
+        public void AdicionarTarefa(string descricao)
+        {
+            var tarefa = new Tarefa
+            {
+                Id = ProximoId++,
+                Descricao = descricao,
+                Concluida = false
+            };
+
+            Tarefas.Add(tarefa);
+        }
+
+        public void AtualizarTarefa(int id, string descricao, bool concluido)
+        {
+            var tarefa = Tarefas.FirstOrDefault(t => t.Id == id);
+
+            if (tarefa == null)
+            {
+                tarefa.Descricao = descricao;
+                tarefa.Concluida = concluido;
+            }
+        }
+
+        public void ConcluirTarefa(int id)
+        {
+            var tarefa = Tarefas.FirstOrDefault(t => t.Id == id);
+
+            tarefa.Concluida = true;
+        }
+
+        public void RemoverTarefa(int id)
+        {
+            var tarefa = Tarefas.FirstOrDefault(t => t.Id == id);
+
+            Tarefas.Remove(tarefa);
+        }
+
+        public List<Tarefa> ListarTarefas()
+        {
+            return Tarefas;
+        }
+    }
+}
---
+++ ToDoList/Program.cs
@@ -0,0 +1,98 @@
+﻿using ToDoList.Classes;
+
+class Program
+{
+   
+    static void Main(string[] args)
+    {
+        GerenciadorDeTarefas gerenciador = new GerenciadorDeTarefas();
+
+        void ExibirLogo()
+        {
+            Console.WriteLine("""
+
+    ████████╗░█████╗░  ██████╗░░█████╗░  ██╗░░░░░██╗░██████╗████████╗
+    ╚══██╔══╝██╔══██╗  ██╔══██╗██╔══██╗  ██║░░░░░██║██╔════╝╚══██╔══╝
+    ░░░██║░░░██║░░██║  ██║░░██║██║░░██║  ██║░░░░░██║╚█████╗░░░░██║░░░
+    ░░░██║░░░██║░░██║  ██║░░██║██║░░██║  ██║░░░░░██║░╚═══██╗░░░██║░░░
+    ░░░██║░░░╚█████╔╝  ██████╔╝╚█████╔╝  ███████╗██║██████╔╝░░░██║░░░
+    ░░░╚═╝░░░░╚════╝░  ╚═════╝░░╚════╝░  ╚══════╝╚═╝╚═════╝░░░░╚═╝░░░
+    """);
+        }
+        void ExibirMenu()
+        {
+            Console.WriteLine("1. Adicionar Tarefa");
+            Console.WriteLine("2. Listar Tarefas");
+            Console.WriteLine("3. Atualizar Tarefa");
+            Console.WriteLine("4. Remover Tarefa");
+            Console.WriteLine("5. Remover Tarefa");
+            Console.WriteLine("6. Sair");
+            Console.Write("Escolha uma opção: ");
+            var opcao = Console.ReadLine();
+
+            switch (opcao)
+            {
+                case "1":
+                    Console.Write("Digite a descrição da tarefa: ");
+                    var descricao = Console.ReadLine();
+                    gerenciador.AdicionarTarefa(descricao);
+                    Console.WriteLine($"A tarefa foi registrada com sucesso!");
+                    Thread.Sleep(4000);
+                    Console.Clear();
+                    ExibirMenu();
+                    break;
+                case "2":
+                    var tarefas = gerenciador.ListarTarefas();
+                    foreach (var tarefa in tarefas)
+                    {
+                        Console.WriteLine($"{tarefa.Id}. {tarefa.Descricao} - {(tarefa.Concluida ? "Concluída" : "Pendente")}");
+                    }
+                    Console.WriteLine("Digite uma tecla para votar ao menu principal");
+                    Console.ReadKey();
+                    Console.Clear();
+                    ExibirMenu();
+                    break;
+                case "3":
+                    Console.Write("Digite o ID da tarefa a ser atualizada: ");
+                    var idAtualizar = int.Parse(Console.ReadLine());
+                    Console.Write("Digite a nova descrição: ");
+                    var novaDescricao = Console.ReadLine();
+                    Console.Write("A tarefa está concluída? (s/n): ");
+                    var concluida = Console.ReadLine().ToLower() == "s";
+                    gerenciador.AtualizarTarefa(idAtualizar, novaDescricao, concluida);
+                    Console.WriteLine($"A tarefa foi registrada com sucesso!");
+                    Thread.Sleep(4000);
+                    Console.Clear();
+                    ExibirMenu();
+                    break;
+                case "4":
+                    Console.Write("Digite o ID da tarefa a ser removida: ");
+                    var idRemover = int.Parse(Console.ReadLine());
+                    gerenciador.RemoverTarefa(idRemover);
+                    Console.WriteLine($"A tarefa foi removida com sucesso!");
+                    Thread.Sleep(4000);
+                    Console.Clear();
+                    ExibirMenu();
+                    break;
+                case "5":
+                    Console.WriteLine("Digite o ID da tarefa que deseja concluir");
+                    var idConcluir = int.Parse(Console.ReadLine());
+                    gerenciador.ConcluirTarefa(idConcluir);
+                    Console.WriteLine($"A tarefa foi removida com sucesso!");
+                    Thread.Sleep(4000);
+                    Console.Clear();
+                    ExibirMenu();
+                    break ;
+                case "6":
+                    return;
+                default:
+                    Console.WriteLine("Opção inválida. Tente novamente.");
+                    Thread.Sleep(4000);
+                    Console.Clear();
+                    ExibirMenu();
+                    break;
+            }
+
+        }
+    }
+}
